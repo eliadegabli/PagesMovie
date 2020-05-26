3364e938c2af8fe7097cf226f59464ba290d3360
אליעד דגבלי <eliadegabli@hotmail.com>
2020-05-26 15:19:58 +0300

    ddsdfds

---
+++ Models/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        [Display(Name = "שם הסרט")]
+        public string Title { get; set; }
+
+        [Display(Name = "תאריך יציאה")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "ז'אנר")]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18, 2)")]
+        [Display(Name = "מחיר")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        [Display(Name = "דירוג")]
+        public string Rating { get; set; }
+
+    }
+}
